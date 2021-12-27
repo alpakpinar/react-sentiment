fc28aa307b274385bf2fba7ea137a4f3cec934b1
Alp Akpinar <aakpinar@bu.edu>
2021-12-27 16:21:48 +0300

    Fix the main path

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_restful import Api
 from flask_cors import CORS
 from api.apiHandler import ApiHandler
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='frontend/build')
 
 # CORS(app)
 api = Api(app)
 api.add_resource(ApiHandler, "/api/sentiment", resource_class_kwargs={"sentence" : "This is good!"})
+
+@app.route('/', defaults={'path' : ''})
+def serve(path):
+    return send_from_directory(app.static_folder)
